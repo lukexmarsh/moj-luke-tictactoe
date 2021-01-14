c71b7198ca07e934a0abb92a0aaac8f3391ca80b
Luke Marsh <luke.marsh@cazoo.co.uk>
2021-01-14 10:51:12 +0000

    Add passing test

--- src/main/tic-tac-toe.ts
+++ src/main/tic-tac-toe.ts
@@ -1,16 +1,32 @@
 export class TicTacToe {
   public currentPlayer: string;
-  private playerOne = 'X';
-  private playerTwo = 'O';
+  private playerOne = "X";
+  private playerTwo = "O";
   public board: string[];
+  public winner: string;
 
   constructor() {
     this.currentPlayer = this.playerOne;
-    this.board = ["","","","","","","","",""];
+    this.board = ["", "", "", "", "", "", "", "", ""];
+    this.winner = "";
   }
 
   public play(positionOnBoard: number) {
-    this.board[positionOnBoard] = this.currentPlayer;
-    this.currentPlayer = this.currentPlayer === this.playerOne ? this.playerTwo : this.playerOne;
+    if (this.board[positionOnBoard] === "") {
+      this.board[positionOnBoard] = this.currentPlayer;
+
+      if (
+        this.board[0] === "X" &&
+        this.board[1] === "X" &&
+        this.board[2] === "X"
+      ) {
+        this.winner = "X";
+      }
+
+      this.currentPlayer =
+        this.currentPlayer === this.playerOne ? this.playerTwo : this.playerOne;
+    } else {
+      return new Error("Position has already been filled");
+    }
   }
 }
--- src/test/tic-tac-toe.test.ts
+++ src/test/tic-tac-toe.test.ts
@@ -20,7 +20,7 @@ describe("tic-tac-toe", () => {
     it("should set the next player as X", () => {
       const ticTacToe = new TicTacToe();
       ticTacToe.play(0);
-      ticTacToe.play(0);
+      ticTacToe.play(1);
       expect(ticTacToe.currentPlayer).toEqual("X");
     });
   });
@@ -30,6 +30,29 @@ describe("tic-tac-toe", () => {
       const ticTacToe = new TicTacToe();
       ticTacToe.play(0);
       expect(ticTacToe.board[0]).toEqual("X");
+    });
+  });
+
+  describe("when the second piece is played on 0", () => {
+    it("should not overwrite the value on that board position", () => {
+      const ticTacToe = new TicTacToe();
+      ticTacToe.play(0);
+      expect(ticTacToe.play(0)).toEqual(
+        new Error("Position has already been filled")
+      );
+      expect(ticTacToe.board[0]).toEqual("X");
+    });
+  });
+
+  describe("when the second piece is played on 0", () => {
+    it("should not overwrite the value on that board position", () => {
+      const ticTacToe = new TicTacToe();
+      ticTacToe.play(0);
+      ticTacToe.play(3);
+      ticTacToe.play(1);
+      ticTacToe.play(4);
+      ticTacToe.play(2);
+      expect(ticTacToe.winner).toEqual("X");
     });
   });
 });
