c4c5b2a0d6ec009eef90088b48c1bf7c9d12b42c
Moujib Aghrout <moujib.aghrout@cazoo.co.uk>
2021-01-14 10:03:15 +0000

    Green: wrote tests for alternating players and placing pieces

--- src/main/tic-tac-toe.ts
+++ src/main/tic-tac-toe.ts
@@ -1,11 +1,16 @@
 export class TicTacToe {
   public currentPlayer: string;
+  private playerOne = 'X';
+  private playerTwo = 'O';
+  public board: string[];
 
   constructor() {
-    this.currentPlayer = "X";
+    this.currentPlayer = this.playerOne;
+    this.board = ["","","","","","","","",""];
   }
 
-  public play() {
-    this.currentPlayer = "O";
+  public play(positionOnBoard: number) {
+    this.board[positionOnBoard] = this.currentPlayer;
+    this.currentPlayer = this.currentPlayer === this.playerOne ? this.playerTwo : this.playerOne;
   }
 }
--- src/test/tic-tac-toe.test.ts
+++ src/test/tic-tac-toe.test.ts
@@ -7,11 +7,29 @@ describe("tic-tac-toe", () => {
       expect(ticTacToe.currentPlayer).toEqual("X");
     });
   });
+
   describe("when the first player plays", () => {
-    it("should set the second player as O", () => {
+    it("should set the next player as O", () => {
       const ticTacToe = new TicTacToe();
-      ticTacToe.play();
+      ticTacToe.play(0);
       expect(ticTacToe.currentPlayer).toEqual("O");
+    });
+  });
+
+  describe("when the second player plays", () => {
+    it("should set the next player as X", () => {
+      const ticTacToe = new TicTacToe();
+      ticTacToe.play(0);
+      ticTacToe.play(0);
+      expect(ticTacToe.currentPlayer).toEqual("X");
+    });
+  });
+
+  describe("when the first piece is played on 0", () => {
+    it("should return the correct board value at that position", () => {
+      const ticTacToe = new TicTacToe();
+      ticTacToe.play(0);
+      expect(ticTacToe.board[0]).toEqual("X");
     });
   });
 });
